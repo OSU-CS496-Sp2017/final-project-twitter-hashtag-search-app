b9c3dc1401f7260c494f46b4b2b3d5654375b52f
Bo <zhangboandvictor@hotmail.com>
2017-06-12 19:24:53 -0700

    Add user icon in main activity

--- app/src/main/java/com/example/xiaoli/twitterhashtagsearcher/TwitterSearchAdapter.java
+++ app/src/main/java/com/example/xiaoli/twitterhashtagsearcher/TwitterSearchAdapter.java
@@ -1,13 +1,19 @@
 package com.example.xiaoli.twitterhashtagsearcher;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.xiaoli.twitterhashtagsearcher.utils.TwitterSearchUtils;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
@@ -54,21 +60,50 @@ public class TwitterSearchAdapter extends RecyclerView.Adapter<TwitterSearchAdap
 
     class SearchResultViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mSearchResultTV;
+        private ImageView mSearchResultIV;
 
         public SearchResultViewHolder(View itemView) {
             super(itemView);
             mSearchResultTV = (TextView)itemView.findViewById(R.id.tv_search_result);
+            mSearchResultIV = (ImageView) itemView.findViewById(R.id.iv_user_profile_image);
             itemView.setOnClickListener(this);
         }
 
         public void bind(TwitterSearchUtils.SearchResult searchResult) {
             mSearchResultTV.setText(searchResult.Text);
+
+            new DownloadImageTask(mSearchResultIV).execute(searchResult.Profile_image);
         }
 
         @Override
         public void onClick(View v) {
             TwitterSearchUtils.SearchResult searchResult = mSearchResultsList.get(getAdapterPosition());
             mSearchResultClickListener.onSearchResultClick(searchResult);
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
         }
     }
 }
